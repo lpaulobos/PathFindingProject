ffeaade9a68a8183512d5afa2c00b0421dbd10c0
Luís Paulo Braga de Oliveira da Silva <lpaulobos2010@gmail.com>
2016-09-01 12:13:46 -0300

    DO IT

---
+++ Pathfinding Project/Assets/Script/Grid.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Grid : MonoBehaviour {
+
+	public int row;
+	public int column;
+	public GameObject prefab;
+	public GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+	
+
+		for (int i = 0; i < column; i++) {
+			int y = 0;
+			for (int j = 0; j < row; j++){
+
+				GameObject createImage = Instantiate (prefab) as GameObject;
+				createImage.transform.SetParent (canvas.transform, false);
+				createImage.transform.position = new Vector2(createImage.transform.position.x + i*2,
+				                                             createImage.transform.position.y + i*y);
+			}
+			y += 2;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
